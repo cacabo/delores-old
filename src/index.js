92f683ecac1dc57ba774c9b1262a7ecd9206c01f
ccabo1 <ccabo@seas.upenn.edu>
2018-06-26 21:35:48 -0700

    updating dependencies

--- src/index.js
+++ src/index.js
@@ -4,8 +4,5 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './assets/scss/index.scss';
 
-console.log('node jawn');
-console.log(process);
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
