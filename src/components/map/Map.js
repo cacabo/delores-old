85dbaf04313fed837847e6cfec2e6478a379bf1b
ccabo1 <ccabo@seas.upenn.edu>
2018-08-24 14:31:12 -0700

    Change how map populated

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,7 +1,53 @@
-import React from 'react';
+/* global google */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-const { REACT_APP_GOOGLE_KEY } = process.env;
+class Map extends Component {
+  componentDidMount() {
+    const { lat, lng } = this.props;
+    const position = { lat, lng };
 
-export default () => (
-  <div id="map" />
-);
+    let map;
+
+    function initMap() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: position,
+        zoom: 8,
+      });
+
+      new google.maps.Marker({
+        position,
+        map,
+      });
+    }
+
+    function waitForGoogle() {
+      if (typeof google !== 'undefined') {
+        initMap();
+      } else {
+        // Check again if google is defined
+        setTimeout(waitForGoogle, 125);
+      }
+    }
+
+    waitForGoogle();
+  }
+
+  render() {
+    return (
+      <div id="map"></div>
+    );
+  }
+}
+
+Map.defaultProps = {
+  lat: -34.397,
+  lng: 150.644,
+};
+
+Map.propTypes = {
+  lat: PropTypes.number,
+  lng: PropTypes.number,
+};
+
+export default Map;
